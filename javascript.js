20abc1636a814a719d41e8a076008df236b3523e
andreshko92 <46976661+andreshko92@users.noreply.github.com>
2026-02-03 11:04:14 +0200

    I have added the prompt for the human's choice. Additionally two new variable were added to keep the human's nad computer's score.

--- javascript.js
+++ javascript.js
@@ -1,18 +1,22 @@
 
 
-// The computer choice should randomly return string values “rock”, “paper” or “scissors”
-
-
+// 1. The computer choice should randomly return string values “rock”, “paper” or “scissors”
 
 const getComputerChoice = () => {
     const result = Math.floor(Math.random() * 3);
     if (result === 0) {
-        return "Rock";
+        return "rock";
     } else if (result === 1) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 };
-/* console.log(getComputerChoice());
- */
+
+// 2. Added the prompt which will acquire human's choice.
+const getHumanChoice = prompt("Choose and write one of the below options:\nRock\nPaper\nScissors");
+
+// 3. Your game will keep track of the players score. You will write variables to keep track of the players score.
+
+let humanScore = 0;
+let computerScore = 0;
